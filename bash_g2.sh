1990e4e7191d4368335488dd5fbcc6e50e75b910
masteredward <mickey_blackmore@hotmail.com>
2016-12-01 01:03:10 -0200

    Versão 0.4
    Seleção de origem e destino, correções nos script.

--- bash_g2.sh
+++ bash_g2.sh
@@ -7,8 +7,8 @@ cp_mode=upload # Modo de operação. upload = Host Local -> Host Remoto, downloa
 cp_tool=rsync # Ferramenta
 ssh_port=22 # Porta SSH do Host Remoto
 ssh_user=root # Usuário do Host Remoto
-local_path=~ # Caminho Absoluto Local
-remote_path=~ # Caminho Absoluto Remoto
+src_path=~ # Caminho Absoluto Local
+dest_path=~ # Caminho Absoluto Remoto
 remote_host=localhost # Host Remoto Padrão
 
 # Função do Cabeçalho do Menu
@@ -24,8 +24,10 @@ function fn_header {
 function fn_change_mode {
 	clear
 	echo "Escolha a modo de operação:"
+	echo
 	echo "1- upload (Host Local -> Host Remoto)"
 	echo "2- download (Host Remoto -> Host Local)"
+	echo
 	echo
 	echo -n "Digite o número do modo desejado: "
 	read opt_mode
@@ -45,8 +47,10 @@ function fn_change_mode {
 function fn_change_tool {
 	clear
 	echo "Escolha uma das ferramentas abaixo:"
+	echo
 	echo "1- rsync (Sincroniza)"
 	echo "2- scp (Sobrescreve)"
+	echo
 	echo
 	echo -n "Digite o número da ferramenta desejada: "
 	read opt_tool
@@ -65,7 +69,7 @@ function fn_change_tool {
 # Função de Seleção da Porta SSH
 function fn_change_port {
 	clear
-	echo "Digite a porta SSH do host remoto:"
+	echo "Escolha a porta SSH do host remoto:"
 	echo
 	echo "Atualmente: $ssh_port"
 	echo
@@ -77,7 +81,7 @@ function fn_change_port {
 # Função de Seleção de Host Remoto
 function fn_change_host {
 	clear
-	echo "Digite o endereço do host remoto:"
+	echo "Escolha o endereço do host remoto:"
 	echo
 	echo "Atualmente: $remote_host"
 	echo
@@ -89,13 +93,37 @@ function fn_change_host {
 # Função de Seleção do Usuário do Host Remoto
 function fn_change_user {
 	clear
-	echo "Digite o nome do usuário do host remoto:"
+	echo "Escolha o nome do usuário do host remoto:"
 	echo
 	echo "Atualmente: $ssh_user"
 	echo
 	echo
-	echo -n "Digite o nome do usuário do host remoto desejado "
+	echo -n "Digite o nome do usuário do host remoto desejado: "
 	read ssh_user
+}
+
+# Função de Seleção do Caminho de Origem
+function fn_change_src_path {
+	clear
+	echo "Escolha o caminho absoluto do arquivo ou diretório a ser copiado:"
+	echo
+	echo "Atualmente: $src_path"
+	echo
+	echo
+	echo -n "Digite o caminho de origem desejado: "
+	read src_path
+}
+
+# Função de Seleção do Caminho de Destino
+function fn_change_src_path {
+	clear
+	echo "Escolha o caminho absoluto do destino da cópia:"
+	echo
+	echo "Atualmente: $dest_path"
+	echo
+	echo
+	echo -n "Digite o caminho de destino desejado: "
+	read dest_path
 }
 
 # Função de Exibição do Menu
@@ -104,7 +132,7 @@ function fn_show_menu {
 	echo
 	echo "-- Menu de Cópia --"
 	PS3='Escolha uma das opções: '
-	options=("Selecionar o modo de operação ($cp_mode)" "Selecionar a ferramenta ($cp_tool)" "Selecionar a porta destino SSH ($ssh_port)" "Selecionar o endereço do host remoto ($remote_host)" "Selecionar o usuário do host remoto ($ssh_user)" "Caminho absoluto de origem ($local_path)" "Caminho absoluto de destino ($remote_path)" "Efetuar a cópia!" "Cancelar e sair do script")
+	options=("Selecionar o modo de operação ($cp_mode)" "Selecionar a ferramenta ($cp_tool)" "Selecionar a porta destino SSH ($ssh_port)" "Selecionar o endereço do host remoto ($remote_host)" "Selecionar o usuário do host remoto ($ssh_user)" "Caminho absoluto de origem ($src_path)" "Caminho absoluto de destino ($dest_path)" "Efetuar a cópia!" "Cancelar e sair do script")
 	select opt in "${options[@]}"
 	do
 		case $opt in
@@ -128,10 +156,12 @@ function fn_show_menu {
 				fn_change_user
 				fn_show_menu
 				;;
-			"Caminho absoluto de origem ($local_path)")
+			"Caminho absoluto de origem ($src_path)")
+				fn_change_src_path
 				fn_show_menu
 				;;
-			"Caminho absoluto de destino ($remote_path)")
+			"Caminho absoluto de destino ($dest_path)")
+				fn_change_dest_path
 				fn_show_menu
 				;;
 			"Efetuar a cópia!")
